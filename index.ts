54a39de5e415faf2bf873f5c145fb80e5575e302
Slava <110696118+sl7one@users.noreply.github.com>
2023-09-06 19:16:48 +0300

    init

---
+++ index.ts
@@ -0,0 +1,229 @@
+//utils
+
+const log = (result: any): void => {
+  console.log(result);
+};
+
+//1. DeepEqual
+console.log("1. DeepEqual");
+const deepEqual = (val1: object, val2: object): boolean => {
+  if (JSON.stringify(val1) === JSON.stringify(val2)) {
+    return true;
+  }
+  return false;
+};
+
+log(deepEqual({ name: "test" }, { name: "test" }));
+log(deepEqual({ name: "test" }, { name: "test1" }));
+log(
+  deepEqual(
+    { name: "test", data: { value: 1 } },
+    { name: "test", data: { value: 2 } }
+  )
+);
+log(deepEqual({ name: "test" }, { name: "test", age: 10 }));
+console.log("_____________________________");
+console.log("");
+
+//2. chunkArray
+console.log("2. chunkArray");
+
+type ChunkArrayType = {
+  next: () => { value: any[] | undefined; done: boolean };
+};
+
+const chunkArray = (array: any[], counter: number): ChunkArrayType => {
+  let idx: number = 0;
+  return {
+    next: () => {
+      const start: number = idx * counter;
+      const end: number = start + counter;
+
+      if (start < array.length) {
+        const result = { value: array.slice(start, end), done: false };
+        idx++;
+        return result;
+      } else {
+        return { value: undefined, done: true };
+      }
+    },
+  };
+};
+
+const iterator = chunkArray([1, 2, 3, 4, 5, 6, 7, 8], 3);
+log(iterator.next());
+log(iterator.next());
+log(iterator.next());
+log(iterator.next());
+
+console.log("_____________________________");
+console.log("");
+
+//3. bulkRun
+const f1 = (cb: (val: any) => void) => {
+  return cb(1);
+};
+
+const f2 = (a: any, cb: (val: any) => void) => {
+  return cb(a);
+};
+
+const f3 = (a: any, b: any, cb: (val: any) => void) => {
+  setTimeout(() => {
+    return cb([a, b]);
+  }, 1000);
+};
+
+type ParamItem = [(...args: any[]) => void, any[]];
+type ParamsType = ParamItem[];
+
+function bulkRun(arr: ParamsType) {
+  const promises = arr.map(([fn, args]) => {
+    return new Promise(res => {
+      fn(...args, res);
+    });
+  });
+
+  return Promise.all(promises);
+}
+
+bulkRun([
+  [f1, []],
+  [f2, [2]],
+  [f3, [3, 4]],
+]).then(r => {
+  console.log("3. bulkRun");
+  log(r);
+  console.log("_____________________________");
+  console.log("");
+});
+
+console.log("_____________________________");
+console.log("");
+
+//4. arrayToObject
+console.log("4. arrayToObject");
+
+const arr = [
+  ["name", "developer"],
+  ["age", 5],
+  [
+    "skills",
+    [
+      ["html", 4],
+      ["css", 5],
+      ["js", 5],
+    ],
+  ],
+];
+
+const arrayToObject = (array: any[]): object => {
+  let res = {};
+
+  array.forEach(([name, value]: [name: string, value: string | []]) => {
+    if (Array.isArray(value))
+      return (res = { ...res, [name]: arrayToObject(value) });
+
+    res = { ...res, [name]: value };
+  });
+
+  return res;
+};
+
+log(arrayToObject(arr));
+
+console.log("_____________________________");
+console.log("");
+
+//5. objectToArray
+console.log("5. objectToArray");
+
+const objectToArray = (obj: object): any[] => {
+  let res: any[] = [];
+  const entries = Object.entries(obj);
+
+  entries.forEach(([key, value]: [key: string, value: string | object]) => {
+    if (typeof value === "object") return res.push([key, objectToArray(value)]);
+
+    res.push([key, value]);
+  });
+
+  return res;
+};
+
+log(
+  objectToArray({
+    name: "developer",
+    age: 5,
+    skills: {
+      html: 4,
+      css: 5,
+      js: 5,
+    },
+  })
+);
+
+console.log("_____________________________");
+console.log("");
+
+//6. reliableMultiply
+console.log("6. reliableMultiply");
+
+function NotificationException(): string {
+  return "notification";
+}
+function ErrorException(): string {
+  return "error";
+}
+function primitiveMultiply(a: number, b: number) {
+  const rand = Math.random();
+  if (rand < 0.5) {
+    return a * b;
+  } else if (rand > 0.85) {
+    throw ErrorException();
+  } else {
+    throw NotificationException();
+  }
+}
+
+function reliableMultiply(a: number, b: number) {
+  try {
+    return primitiveMultiply(a, b);
+  } catch (error) {
+    if (error === "notification") {
+      return reliableMultiply(a, b);
+    } else {
+      throw error;
+    }
+  }
+}
+
+log(reliableMultiply(8, 8));
+
+console.log("_____________________________");
+console.log("");
+
+//9. Add
+console.log("9. Add");
+
+const add = (x: number) => {
+  let sum = x;
+
+  const inner = (y: number) => {
+    sum += y;
+    return inner;
+  };
+
+  inner.valueOf = () => {
+    return sum;
+  };
+
+  return inner;
+};
+
+log(Number(add(1)(2)));
+log(Number(add(1)(2)(5)));
+log(Number(add(1)(2)(-3)(4)));
+log(Number(add(1)(2)(3)(4)(-5)));
+console.log("_____________________________");
+console.log("");
---
+++ js/index.js
@@ -0,0 +1,174 @@
+"use strict";
+const log = (result) => {
+    console.log(result);
+};
+console.log("1. DeepEqual");
+const deepEqual = (val1, val2) => {
+    if (JSON.stringify(val1) === JSON.stringify(val2)) {
+        return true;
+    }
+    return false;
+};
+log(deepEqual({ name: "test" }, { name: "test" }));
+log(deepEqual({ name: "test" }, { name: "test1" }));
+log(deepEqual({ name: "test", data: { value: 1 } }, { name: "test", data: { value: 2 } }));
+log(deepEqual({ name: "test" }, { name: "test", age: 10 }));
+console.log("_____________________________");
+console.log("");
+console.log("2. chunkArray");
+const chunkArray = (array, counter) => {
+    let idx = 0;
+    return {
+        next: () => {
+            const start = idx * counter;
+            const end = start + counter;
+            if (start < array.length) {
+                const result = { value: array.slice(start, end), done: false };
+                idx++;
+                return result;
+            }
+            else {
+                return { value: undefined, done: true };
+            }
+        },
+    };
+};
+const iterator = chunkArray([1, 2, 3, 4, 5, 6, 7, 8], 3);
+log(iterator.next());
+log(iterator.next());
+log(iterator.next());
+log(iterator.next());
+console.log("_____________________________");
+console.log("");
+const f1 = (cb) => {
+    return cb(1);
+};
+const f2 = (a, cb) => {
+    return cb(a);
+};
+const f3 = (a, b, cb) => {
+    setTimeout(() => {
+        return cb([a, b]);
+    }, 1000);
+};
+function bulkRun(arr) {
+    const promises = arr.map(([fn, args]) => {
+        return new Promise(res => {
+            fn(...args, res);
+        });
+    });
+    return Promise.all(promises);
+}
+bulkRun([
+    [f1, []],
+    [f2, [2]],
+    [f3, [3, 4]],
+]).then(r => {
+    console.log("3. bulkRun");
+    log(r);
+    console.log("_____________________________");
+    console.log("");
+});
+console.log("_____________________________");
+console.log("");
+console.log("4. arrayToObject");
+const arr = [
+    ["name", "developer"],
+    ["age", 5],
+    [
+        "skills",
+        [
+            ["html", 4],
+            ["css", 5],
+            ["js", 5],
+        ],
+    ],
+];
+const arrayToObject = (array) => {
+    let res = {};
+    array.forEach(([name, value]) => {
+        if (Array.isArray(value))
+            return (res = Object.assign(Object.assign({}, res), { [name]: arrayToObject(value) }));
+        res = Object.assign(Object.assign({}, res), { [name]: value });
+    });
+    return res;
+};
+log(arrayToObject(arr));
+console.log("_____________________________");
+console.log("");
+console.log("5. objectToArray");
+const objectToArray = (obj) => {
+    let res = [];
+    const entries = Object.entries(obj);
+    entries.forEach(([key, value]) => {
+        if (typeof value === "object")
+            return res.push([key, objectToArray(value)]);
+        res.push([key, value]);
+    });
+    return res;
+};
+log(objectToArray({
+    name: "developer",
+    age: 5,
+    skills: {
+        html: 4,
+        css: 5,
+        js: 5,
+    },
+}));
+console.log("_____________________________");
+console.log("");
+console.log("6. reliableMultiply");
+function NotificationException() {
+    return "notification";
+}
+function ErrorException() {
+    return "error";
+}
+function primitiveMultiply(a, b) {
+    const rand = Math.random();
+    if (rand < 0.5) {
+        return a * b;
+    }
+    else if (rand > 0.85) {
+        throw ErrorException();
+    }
+    else {
+        throw NotificationException();
+    }
+}
+function reliableMultiply(a, b) {
+    try {
+        return primitiveMultiply(a, b);
+    }
+    catch (error) {
+        if (error === "notification") {
+            return reliableMultiply(a, b);
+        }
+        else {
+            throw error;
+        }
+    }
+}
+log(reliableMultiply(8, 8));
+console.log("_____________________________");
+console.log("");
+console.log("9. Add");
+const add = (x) => {
+    let sum = x;
+    const inner = (y) => {
+        sum += y;
+        return inner;
+    };
+    inner.valueOf = () => {
+        return sum;
+    };
+    return inner;
+};
+log(Number(add(1)(2)));
+log(Number(add(1)(2)(5)));
+log(Number(add(1)(2)(-3)(4)));
+log(Number(add(1)(2)(3)(4)(-5)));
+console.log("_____________________________");
+console.log("");
+//# sourceMappingURL=index.js.map
